7d4081a3c3ff337b8ed13948f47037c28fc06f5a
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2017-04-09 22:38:06 +1200

    Added a helper to make anchors in text

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,18 @@
 <?php
 
+function str_make_links($text) {
+    $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+
+    // Check if there is a url in the text
+    if (preg_match($reg_exUrl, $text, $url)) {
+        // make the urls hyper links
+        return preg_replace($reg_exUrl, '<a href="'.$url[0].'" rel="nofollow">'.$url[0].'</a>', $text);
+    }
+    else {
+        return $text;
+    }
+}
+
 function is_assoc(array $arr)
 {
     if (array() === $arr) return false;
@@ -89,7 +102,7 @@ function validateDate($date, $format = null) {
     if (is_null($format) && defined('DATE_FORMAT')) {
         $format = DATE_FORMAT;
     }
-    
+
     $d = \DateTime::createFromFormat($format, $date);
     return $d && $d->format($format) === $date;
 }
